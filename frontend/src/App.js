12de453cd60b03906e10f08b6fe6614f611a541b
Adam Pangelinan <adamtpangelinan@gmail.com>
2023-03-03 12:12:22 -0800

    Add grocery store project basic css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
 // import LoginFormPage from "./components/LoginFormPage";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/Navigation/Navigation";
 import ItemIndex from "./components/ItemIndex/ItemIndex";
+import Cart from './components/Cart';
+import ProduceList from './components/ProduceList';
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
   return (
     <>
+      <>
+        <nav>
+          <h1>Grocery Store</h1>
+          <button className="checkout-button" onClick={() => setShowCart(true)}>
+            <i className="fas fa-shopping-bag" />
+            Checkout
+          </button>
+        </nav>
+        <main style={showCart ? { marginRight: '300px' } : {}} >
+          <ProduceList />
+        </main>
+        <div
+          className="sidebar"
+          style={showCart ? { transform: 'translateX(-100%)' } : {}}
+        >
+          <div className="sidebar-header">
+            <button className="arrow-button" onClick={() => setShowCart(false)}>
+              <i className="fas fa-arrow-right"></i>
+            </button>
+          </div>
+          <Cart />
+        </div>
+      </>
       <Navigation />
         <Switch>
           {/* <Route path="/login" >
