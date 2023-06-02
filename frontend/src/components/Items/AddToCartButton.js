fb9d57925a44a185aed7819a7f4420e6ab3d887b
Adam Pangelinan <adamtpangelinan@gmail.com>
2023-06-02 21:14:47 +1000

    Change render postgres db

--- frontend/src/components/Items/AddToCartButton.js
+++ frontend/src/components/Items/AddToCartButton.js
@@ -4,40 +4,44 @@ import { useParams } from "react-router-dom";
 import { addCartItemThunk, updateCartItemThunk } from "../../store/cartItem";
 import "./AddToCartButton.css";
 
-export default function AddToCartButton ({ item }) {
-    const dispatch = useDispatch();
-    const currentUser = useSelector(state => state.session.user) || {};
-    const { storeId } = useParams();
-    
-    const cartItems = useSelector(state => Object.values(state.cartItems));
-    const cartItem = {
-        user_id: currentUser.id,
-        store_id: parseInt(storeId),
-        item_id: item.id,
-        quantity: 1
-    };
-    
-    const itemInCart = cartItems.find((cartItem) => cartItem.itemId === item.id && cartItem.userId === currentUser.id);
-    
-    const [clicked, setClicked] = useState(false);
-    const handleClick = async (e) => {
-        e.preventDefault();
-        try {
-            if (itemInCart) {
-                await dispatch(updateCartItemThunk(itemInCart.id, itemInCart.quantity));
-            } else {
-                await dispatch(addCartItemThunk(cartItem));
-            }
-            setClicked(true);
-            } catch (err) {
-            console.error(err);
-        }
-    };
-    
+export default function AddToCartButton({ item }) {
+	const dispatch = useDispatch();
+	const currentUser = useSelector((state) => state.session.user) || {};
+	const { storeId } = useParams();
 
-    return (
-        <button className="addtocart-button" onClick={handleClick}>
-            {clicked ? "Added!" : "Add"}
-        </button>
-    );
-};
+	const cartItems = useSelector((state) => Object.values(state.cartItems));
+	const cartItem = {
+		user_id: currentUser.id,
+		store_id: parseInt(storeId),
+		item_id: item.id,
+		quantity: 1,
+	};
+
+	const itemInCart = cartItems.find(
+		(cartItem) =>
+			cartItem.itemId === item.id && cartItem.userId === currentUser.id
+	);
+
+	const [clicked, setClicked] = useState(false);
+	const handleClick = async (e) => {
+		e.preventDefault();
+		try {
+			if (itemInCart) {
+				await dispatch(
+					updateCartItemThunk(itemInCart.id, itemInCart.quantity)
+				);
+			} else {
+				await dispatch(addCartItemThunk(cartItem));
+			}
+			setClicked(true);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	return (
+		<button className="addtocart-button" onClick={handleClick}>
+			{clicked ? "Added!" : "Add"}
+		</button>
+	);
+}
